d7f16275372150853f121524f36fe4cf6f97c7af
areynen <alexreynen@gmail.com>
2024-06-07 09:07:25 -0700

    add initial test for other pages

---
+++ tests/test_ui/test_pages.py
@@ -0,0 +1,42 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.firefox import GeckoDriverManager
+from flask import Flask
+import time
+from multiprocessing import Process
+from app import run_flask
+
+
+@pytest.fixture(scope='session')
+def flask_init():
+    # Start the Flask app in a separate process
+    port = 5000  # Change the port number here if needed
+    app_process = Process(target=run_flask, args=(port,))
+    app_process.start()
+
+    # Wait for the server to start
+    time.sleep(3)
+
+    yield
+
+    # Teardown
+    app_process.terminate()
+    app_process.join()
+
+
+@pytest.fixture
+def browser():
+    driver = webdriver.Firefox()
+    yield driver
+    driver.quit()
+
+
+def test_view_page(flask_init, browser):
+    browser.get("localhost:5000/view")
+    assert browser.title == "Book Table"
+
+
+def test_view_page(flask_init, browser):
+    browser.get("localhost:5000/scan")
+    assert browser.title == "Barcode Scanner"
