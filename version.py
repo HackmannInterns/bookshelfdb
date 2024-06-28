459d189391d9ebe6b151e8188ab04fe33c6aad55
areynen <alexreynen@gmail.com>
2024-06-28 16:32:23 -0700

    Finish or approx finish version, still need to change the url

--- version.py
+++ version.py
@@ -10,23 +10,25 @@ APP_VERSION = "0.9"
 
 def threaded_check():
     while True:
+        version_info['current'] = '10.9.6'
+        # version_info['current'] = APP_VERSION
+
         # TODO: Change with our link :)
         r = requests.get("https://github.com/jellyfin/jellyfin/releases.atom")
         if r.status_code == 200:
             parsed = ET.fromstring(r.text)
             newest = parsed.find("{http://www.w3.org/2005/Atom}entry")
-
-            title = newest.find("{http://www.w3.org/2005/Atom}title").text
-            link = newest.find(
-                "{http://www.w3.org/2005/Atom}link").attrib['href']
-            # updated = newest.find("{http://www.w3.org/2005/Atom}updated").text
-
-            version_info['newest'] = title
-            version_info['current'] = '10.9.7'
-            # version_info['current'] = APP_VERSION
-            version_info['newest_link'] = link
+            if newest is not None:
+                title = newest.find("{http://www.w3.org/2005/Atom}title").text
+                link = newest.find(
+                    "{http://www.w3.org/2005/Atom}link").attrib['href']
+                # updated = newest.find("{http://www.w3.org/2005/Atom}updated").text
+                version_info['newest_link'] = link
+                version_info['newest'] = title
         else:
-            # set it to something we can use in admin.html to move it
+            version_info['newest_link'] = None
+            version_info['newest'] = None
+
             print(f"resp is {r.status_code}")
         time.sleep(6*60*60)
 
