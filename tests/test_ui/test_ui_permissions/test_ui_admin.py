71f66106dcbe24c3f66ecc5d0acbb2849af68d18
areynen <alexreynen@gmail.com>
2024-07-16 10:50:04 -0700

    start UI permissions tests

--- tests/test_ui/test_ui_permissions/test_ui_admin.py
+++ tests/test_ui/test_ui_permissions/test_ui_admin.py
@@ -7,7 +7,6 @@ from selenium.webdriver.firefox.service import Service
 import time
 from multiprocessing import Process
 from app import run_flask
-from admin import get_settings
 from selenium.common.exceptions import NoSuchDriverException
 
 
@@ -50,6 +49,7 @@ def browser():
 
 
 def login(browser, password):
+    browser.get("localhost:5000/")
     browser.get("localhost:5000/login")
     pass_input = browser.find_element(By.NAME, "password")
     pass_input.clear()
@@ -58,48 +58,46 @@ def login(browser, password):
     time.sleep(3)
 
 
-def test_view_page(flask_init, browser):
+def test_admin_can_view(flask_init, browser):
     from app import ADMIN_PASSWORD
     login(browser, ADMIN_PASSWORD)
+    time.sleep(1)
     browser.get("localhost:5000/")
     assert browser.title == "Library"
+    assert browser.title != "Login Required"
 
 
-def test_admin_settings(browser):
+def test_admin_can_add(flask_init, browser):
     from app import ADMIN_PASSWORD
     login(browser, ADMIN_PASSWORD)
-    browser.get('localhost:5000/admin')
-    time.sleep(3)
-    editor_checkbox = browser.find_element(By.ID, "editor")
-    viewer_checkbox = browser.find_element(By.ID, "viewer")
-    assert editor_checkbox.is_selected() == get_settings().editor_can_remove
-    assert viewer_checkbox.is_selected() == get_settings().visitor_can_add
-    address_box = browser.find_element(By.ID, "address")
-    header_box = browser.find_element(By.ID, "header_name")
-    assert address_box.get_attribute('value') == get_settings().default_address
-    assert header_box.get_attribute('value') == get_settings().header_name
-
-
-def test_add_book_page(flask_init, browser):
-    from app import ADMIN_PASSWORD
-    login(browser, ADMIN_PASSWORD)
-    browser.get("localhost:5000/")
+    time.sleep(1)
     browser.get("localhost:5000/add-book")
     assert browser.title == "Add Book"
+    assert browser.title != "Login Required"
 
 
-# Requires an entry, not sure how we wanna handle this
-# def test_edit_page_with_admin(flask_init, browser):
-#     from app import ADMIN_PASSWORD
-#     login(browser, ADMIN_PASSWORD)
-#     browser.get("localhost:5000/edit?q=1")
-#     assert browser.title == "Library"
-
-
-def test_delete_page_with_admin(flask_init, browser):
+def test_admin_can_edit(flask_init, browser):
     from app import ADMIN_PASSWORD
-    # Redirects regardless of ability to delete (in this case, we don't ever have a -1)
     login(browser, ADMIN_PASSWORD)
-    browser.get("localhost:5000/")
-    browser.get("localhost:5000/delete?q=-1")
+    time.sleep(1)
+    browser.get("http://localhost:5000/edit?q=-1")
     assert browser.title == "Library"
+    assert browser.title != "Login Required"
+
+
+def test_admin_can_delete(flask_init, browser):
+    from app import ADMIN_PASSWORD
+    login(browser, ADMIN_PASSWORD)
+    time.sleep(1)
+    browser.get("http://localhost:5000/delete?q=-1")
+    assert browser.title == "Library"
+    assert browser.title != "Login Required"
+
+
+def test_admin_can_view_admin(flask_init, browser):
+    from app import ADMIN_PASSWORD
+    login(browser, ADMIN_PASSWORD)
+    time.sleep(1)
+    browser.get("http://localhost:5000/admin")
+    assert browser.title == "Settings"
+    assert browser.title != "Login Required"
