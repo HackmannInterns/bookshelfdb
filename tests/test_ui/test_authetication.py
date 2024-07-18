8a4bc5983d6ea5be803f1066ae74b31efaf8daac
Alex Reynen <alexreynen@gmail.com>
2024-07-18 13:45:22 -0700

    Fix test_authentication for issues as outlined in the trello

--- tests/test_ui/test_authetication.py
+++ tests/test_ui/test_authetication.py
@@ -7,11 +7,25 @@ from selenium.webdriver.firefox.service import Service
 import time
 from multiprocessing import Process
 from app import run_flask
-from admin import get_settings
+import shutil
+from admin import get_settings, update_yaml
 from selenium.common.exceptions import NoSuchDriverException
+
 
 @pytest.fixture(scope='session')
 def flask_init():
+    # Move data
+    import os
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    data_path = os.path.join(script_dir, "../../../data")
+    backup_path = os.path.join(script_dir, "../../../data-BAK")
+
+    while os.path.exists(backup_path):
+        time.sleep(1)
+
+    if os.path.exists(data_path):
+        os.rename(data_path, backup_path)
+
     # Start the Flask app in a separate process
     port = 5000  # Change the port number here if needed
     app_process = Process(target=run_flask, args=(port,))
@@ -25,6 +39,12 @@ def flask_init():
     # Teardown
     app_process.terminate()
     app_process.join()
+
+    if os.path.exists(backup_path):
+        if os.path.exists(data_path):
+            shutil.rmtree(data_path)
+        os.rename(backup_path, data_path)
+
 
 @pytest.fixture(scope="module")
 def browser():
@@ -46,31 +66,38 @@ def browser():
         yield driver
         driver.quit()
 
-def login(browser, password):
-    browser.get("localhost:5000/login")
+
+def login_wihtout_moving(browser, password):
     pass_input = browser.find_element(By.NAME, "password")
     pass_input.clear()
     pass_input.send_keys(password)
     pass_input.send_keys(Keys.RETURN)
     time.sleep(3)
 
+
 def test_add_perm(flask_init, browser):
+    update_yaml(viewer_can_add=False)
+
     browser.get("http://localhost:5000/logout")
     browser.get("http://localhost:5000/add-book")
     title_text = browser.find_element(By.XPATH, "/html/body/div/h3")
     assert title_text.text == "You cannot add with your current authentication level; Editor or greater required"
-    login(browser, "editor")
+
+    from app import EDITOR_PASSWORD
+    login_wihtout_moving(browser, EDITOR_PASSWORD)
     assert browser.title == "Add Book"
 
+
 def test_remove_perm(flask_init, browser):
+    update_yaml(editor_can_remove=False)
+
     browser.get("http://localhost:5000/logout")
     browser.get("http://localhost:5000/delete?q=-1")
     title_text = browser.find_element(By.XPATH, "/html/body/div/h3")
-    if get_settings().editor_can_remove:
-        assert title_text.text == "You cannot remove with your current authentication level; Editor or greater required"
-    else:
-        assert title_text.text == "You cannot remove with your current authentication level; Admin or greater required"
-    login(browser, "editor")
+    assert title_text.text == "You cannot remove with your current authentication level; Admin or greater required"
+
+    from app import ADMIN_PASSWORD
+    login_wihtout_moving(browser, ADMIN_PASSWORD)
     assert browser.title == "Library"
 
 
@@ -79,14 +106,18 @@ def test_admin_perm(flask_init, browser):
     browser.get("http://localhost:5000/admin")
     title_text = browser.find_element(By.XPATH, "/html/body/div/h3")
     assert title_text.text == "You cannot view admin with your current authentication level; Admin or greater required"
-    login(browser, "admin")
+
+    from app import ADMIN_PASSWORD
+    login_wihtout_moving(browser, ADMIN_PASSWORD)
     assert browser.title == "Settings"
+
 
 def test_edit_perm(flask_init, browser):
     browser.get("http://localhost:5000/logout")
     browser.get("http://localhost:5000/edit?q=-1")
     title_text = browser.find_element(By.XPATH, "/html/body/div/h3")
     assert title_text.text == "You cannot edit with your current authentication level; Editor or greater required"
-    login(browser, "editor")
-    assert browser.title == "Library"
 
+    from app import EDITOR_PASSWORD
+    login_wihtout_moving(browser, EDITOR_PASSWORD)
+    assert browser.title == "Library"
